3d5d9e4a8bbfdafc0ae14ed47bfe1a6304628da2
Avinash <2019avinash2001@gmail.com>
2024-09-03 18:42:23 +0530

    patient login functionality created

--- HMS/patient/views.py
+++ HMS/patient/views.py
@@ -38,7 +38,7 @@ def patient_login(request):
         user = patient_register.objects.filter(Email=email)
         if user:
             for i in user:
-    
+            
                 user_password=i.Password
                 if user_password==password:
                     request.session['user_email'] = email
